2ccd45d7ce6330bc0f37b2b0ecc010be8907551a
Simonsierra07 <simon.sierrav@upb.edu.co>
2026-09-24 00:24:42 +0000

    ejercicio11

---
+++ Projecto 2/Ejercicio11/Ejercicio11.v
@@ -0,0 +1,63 @@
+module Ejercicio11(iSelect,oDisplay1,oDisplay2,oDisplay3,
+ oDisplay4,oDisplay5,oDisplay6);
+input [2:0] iSelect;
+output [6:0] oDisplay1,oDisplay2,oDisplay3,oDisplay4,oDisplay5,oDisplay6;
+wire [3:0] dato1, dato2, dato3, dato4, dato5, dato6;
+wire [5:0] display_n;
+wire [3:0] iA = 4'h2;               // constante = último dígito de tu ID (2)
+mydeco3to6 IC01(display_n,iSelect);
+mymux4to4 IC02(dato1,iA,display_n[0]);
+mymux4to4 IC03(dato2,iA,display_n[1]);
+mymux4to4 IC04(dato3,iA,display_n[2]);
+mymux4to4 IC05(dato4,iA,display_n[3]);
+mymux4to4 IC06(dato5,iA,display_n[4]);
+mymux4to4 IC07(dato6,iA,display_n[5]);
+mydeco_display7 IC08(oDisplay1,dato1);
+mydeco_display7 IC09(oDisplay2,dato2);
+mydeco_display7 IC10(oDisplay3,dato3);
+mydeco_display7 IC11(oDisplay4,dato4);
+mydeco_display7 IC12(oDisplay5,dato5);
+mydeco_display7 IC13(oDisplay6,dato6);
+endmodule
+
+module mydeco_display7(Seg,A);
+input [3:0] A;
+output reg [6:0] Seg; //Seg[6]=a, Seg[5]=b, Seg[4]=c, Seg[3]=d, Seg[2]=e, Seg[1]=f, Seg[0]=g
+always @*
+case (A)
+4'b0000 : Seg <= 7'b0000001; //Hexadecimal 0
+4'b0001 : Seg <= 7'b1001111; //Hexadecimal 1
+4'b0010 : Seg <= 7'b0010010; //Hexadecimal 2
+4'b0011 : Seg <= 7'b0000110; //Hexadecimal 3
+4'b0100 : Seg <= 7'b1001100; //Hexadecimal 4
+4'b0101 : Seg <= 7'b0100100; //Hexadecimal 5
+4'b0110 : Seg <= 7'b0100000; //Hexadecimal 6
+4'b0111 : Seg <= 7'b0001111; //Hexadecimal 7
+4'b1000 : Seg <= 7'b0000000; //Hexadecimal 8
+4'b1001 : Seg <= 7'b0001100; //Hexadecimal 9
+4'b1010 : Seg <= 7'b0001000; //Hexadecimal A
+default : Seg <= 7'b1111111; //Apaga el display
+endcase
+endmodule
+
+module mymux4to4(Y,A,S);
+input [3:0] A;
+input S;
+output [3:0] Y;
+assign Y = S ? A : 4'b1111;
+endmodule
+
+module mydeco3to6(Y,D);
+input [2:0] D;
+output [5:0] Y;
+wire A, B, C;
+wire Anot, Bnot, Cnot;
+buf (A,D[0]); buf (B,D[1]); buf (C,D[2]);
+not (Anot,D[0]); not (Bnot,D[1]); not (Cnot,D[2]);
+and g1(Y[0],Cnot,Bnot,A);
+and g2(Y[1],Cnot,B,Anot);
+and g3(Y[2],Cnot,B,A);
+and g4(Y[3],C,Bnot,Anot);
+and g5(Y[4],C,Bnot,A);
+and g6(Y[5],C,B,Anot);
+endmodule
